9cd380ddecdd40a64e3a685b548bb1e80521bcb1
Ancil Cleetus <ancilcleetus.work@gmail.com>
2022-04-18 19:14:00 +0530

    Added 0001-creation-of-linked-list in ch02-datastructures-in-c/02-linked-list

---
+++ ch02-datastructures-in-c/02-linked-list/0001-creation-of-linked-list/creation-of-linked-list-solution-01.c
@@ -0,0 +1,46 @@
+/*
+** Creates a linked list with 3 nodes
+** and prints data of each node
+** by traversing the linked list.
+*/
+
+// Time Complexity = O(N)
+// Space Complexity = O(N)
+
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{
+    struct node {
+        int data;
+        struct node *next;
+    };
+    
+    struct node *head, *middle, *last;
+    
+    // allocate memory in heap for each node
+    head = malloc(sizeof(struct node));
+    middle = malloc(sizeof(struct node));
+    last = malloc(sizeof(struct node));
+    
+    // initialize data of each node
+    head->data = 10;
+    middle->data = 20;
+    last->data = 30;
+    
+    // create connections between nodes
+    head->next = middle;
+    middle->next = last;
+    last->next = NULL;
+    
+    // print data of each node
+    struct node *temp = head;
+    printf("Traversing the linked list to print data of each node...\n");
+    while (temp != NULL) {
+        printf("%d ", temp->data);
+        temp = temp->next;
+    }
+
+    return 0;
+}
