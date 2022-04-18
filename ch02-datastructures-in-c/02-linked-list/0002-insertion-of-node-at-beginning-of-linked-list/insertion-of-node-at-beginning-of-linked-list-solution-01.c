6f085b3575a0742c0cd1c5a8181264bac69a7fa3
Ancil Cleetus <ancilcleetus.work@gmail.com>
2022-04-18 19:46:58 +0530

    Added 0002-insertion-of-node-at-beginning-of-linked-list in ch02-datastructures-in-c/02-linked-list

---
+++ ch02-datastructures-in-c/02-linked-list/0002-insertion-of-node-at-beginning-of-linked-list/insertion-of-node-at-beginning-of-linked-list-solution-01.c
@@ -0,0 +1,44 @@
+/*
+** Inserts a node at the beginning of a linked list
+*/
+
+// Time Complexity = O(1)
+// Space Complexity = O(1)
+
+#include <stdio.h>
+#include <stdlib.h>
+
+struct node {
+    int data;
+    struct node *next;
+};
+
+struct node *head = NULL;
+
+void addFirst(int val) {
+    struct node *newNode = malloc(sizeof(struct node));
+    newNode->data = val;
+    newNode->next = head;
+    
+    head = newNode;
+}
+
+void printList() {
+    struct node *temp = head;
+    printf("Traversing the linked list to print data of each node...\n");
+    while (temp != NULL) {
+        printf("%d ", temp->data);
+        temp = temp->next;
+    }
+}
+
+int main()
+{
+    addFirst(10);
+    addFirst(20);
+    addFirst(30);
+    
+    printList();
+
+    return 0;
+}
