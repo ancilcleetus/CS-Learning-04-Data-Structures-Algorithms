407aeef62f28ef0ca0e6d1f5121f17020080d7c1
Ancil Cleetus <ancilcleetus.work@gmail.com>
2022-04-21 19:55:38 +0530

    Added 0001-inserting-an-element-in-array to ch03-datastructures-in-python/01-arrays

---
+++ ch03-datastructures-in-python/01-arrays/0001-inserting-an-element-in-array/inserting-an-element-in-array-solution-01.py
@@ -0,0 +1,20 @@
+#!/usr/bin/env python3
+
+# Inserts an element in an array at a particular position.
+
+# Best Case Time Complexity = O(1) => append method
+# Worst Case Time Complexity = O(N) => insertion at first index (insert method)
+# Space Complexity = O(N)
+
+
+if __name__ == '__main__':
+
+    values = [10, 20, 30, 40, 50]
+    
+    values.append(100)
+    print(f"values = {values}")
+    
+    values.insert(0, 5)
+    print(f"values = {values}")
+    
+    
